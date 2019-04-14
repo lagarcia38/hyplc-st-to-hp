8f6a04e9a67aa1fdd47e1051a73f435ef1d6de60
Luis Garcia <garcialuis@ucla.edu>
2019-04-14 02:18:41 -0400

    Base implementation for demo

--- parser/hyplc-st-to-hp.py
+++ parser/hyplc-st-to-hp.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from antlr4 import *
 from IECSTGrammarLexer import IECSTGrammarLexer
 from IECSTGrammarParser import IECSTGrammarParser
@@ -7,30 +8,46 @@ from IECSTExpressions import *
 from IECSTtoHPPrettyPrinter import *
 from typing import List
 
-# Read a number of ST configuration files into this list
-configs: List[ConfigurationFile] = []
+
+# Read an ST configuration file into this structure
+config: ConfigurationFile = None
 
 
 def process_st_file(st_file):
-    input = FileStream(st_file)
-    lexer = IECSTGrammarLexer(input)
-    stream = CommonTokenStream(lexer)
-    parser = IECSTGrammarParser(stream)
-    configFile = parser.configurationfile()
-    configVisitor= IECSTGrammarVisitor()
-    configFile : ConfigurationFile = configVisitor.visit(configFile)
-    print("Generated ConfigurationFile: ",configFile)
-    configs.append(configFile)
-
-
-# TODO: Incorporate continuous dynamics (currently only generates discrete state transitions from ST code)
-#       This would require an auxiliary file with the continuous plant
+	global config
+	input = FileStream(st_file)
+	lexer = IECSTGrammarLexer(input)
+	stream = CommonTokenStream(lexer)
+	parser = IECSTGrammarParser(stream)
+	configFile = parser.configurationfile()
+	configVisitor= IECSTGrammarVisitor()
+	config  = configVisitor.visit(configFile)
 
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='HyPLC ST to HP translation tool.')
+	parser.add_argument('-STFile','--STFile', help='Input ST file to be translated', required=True)
+	parser.add_argument('-HPFile','--HPFile', help='HP file where ctrl will be replaced with translation', required=False)
+
+	args = vars(parser.parse_args())
+
     # Currently only process one ST file with one Program defined at a time
-    process_st_file(sys.argv[1])
-    # Iterate through all structured text programs and generate a hybrid program
-    # (for this implementation, we are only generating one):
-    for config in configs:
-    	printer = IECSTtoHPPrettyPrinter(config)
-    	print(printer.configuration_file_to_string())
+	process_st_file(args['STFile'])
+
+	# Iterate through all structured text programs and generate a hybrid program
+	# (for this implementation, we are only generating one):
+	printer = IECSTtoHPPrettyPrinter(config)
+	ctrl = printer.configuration_file_to_string()
+
+	#If the user specified an input HP file to replace the ctrl: 
+	if args['HPFile'] is not None:
+		hpStr = ""
+		#Add clk info
+		ctrl = ctrl + "\n\tt:= 0; {t'=1&t<e}"
+		with open(args['HPFile'], "rt") as fin:
+			for line in fin:
+				hpStr = hpStr + line.replace('ctrl', ctrl)
+
+		print(hpStr)
+	else:
+		print(ctrl)
+
